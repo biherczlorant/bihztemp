5d802fd2b59017920736503603c3b50e0f32c9fb
Lóránt Bihercz <biherczlori@gmail.com>
2025-03-03 22:00:16 +0100

    display: test shows on ssd1306

--- main/bihztemp_main.c
+++ main/bihztemp_main.c
@@ -1,3 +1,11 @@
+#include <display.h>
+#include <FreeRTOS.h>
+#include <task.h>
+#include <esp_log.h>
 
+int app_main(void){
+    init_display();
+    display_test("test");
+    return 0;
+}
 
-int app_main(void){}
